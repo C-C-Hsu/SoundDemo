ce8f065d2860c63e1dba7a841573df11d0b4f2b2
Allen <Allen>
2016-10-19 19:32:57 +0800

    Button set IBAction(playSound)

--- SoundDemo/ViewController.swift
+++ SoundDemo/ViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
+    @IBAction func playSound(_ sender: AnyObject) {
+        
+        var soundID:SystemSoundID = 0
+        // 取得聲音檔路徑
+        let path = Bundle.main.path(forResource: "msg", ofType: "wav")
+        // 將路徑轉換為 NSURL 格式
+        let baseURL = NSURL(fileURLWithPath: path!)
+        // 將聲音檔取得的 ID 給予 soundID 變數,變數前面加上 & 符號是傳址的意思
+        AudioServicesCreateSystemSoundID(baseURL, &soundID)
+        // 播放聲音
+        AudioServicesPlayAlertSound(soundID)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
